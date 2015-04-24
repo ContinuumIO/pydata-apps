8493f29660c3620b51cf17e302052d5a603d7d0b
Andy R. Terrel <andy.terrel@gmail.com>
2015-04-24 10:23:25 -0500

    Fix odo call

--- embedded_apps/04_baseball_stats/baseball_salaries.py
+++ embedded_apps/04_baseball_stats/baseball_salaries.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 import blaze as bz
-from into import into
+from odo import odo
 
 from bokeh.embed import components
 from bokeh.resources import INLINE
@@ -15,7 +15,7 @@ import bokeh.plotting as plt
 
 
 app = flask.Flask(__name__)
-db = bz.Data('sqlite:///../lahman2013.sqlite')
+db = bz.Data('sqlite:///../../lahman2013.sqlite')
 distinct_teams = list(db.Salaries.teamID.distinct())
 distinct_years = list(db.Salaries.yearID.distinct())
 
@@ -26,19 +26,19 @@ def create_plot(team="LAA", year=2012):
                  ratio=db.Salaries.salary.max() / db.Salaries.salary.min())
     expr = expr.sort('ratio', ascending=False)
 
-    df_salary_gb = into(pd.DataFrame, expr)
-    source1 = into(ColumnDataSource, df_salary_gb[["teamID", "avg"]])
+    df_salary_gb = odo(expr, pd.DataFrame)
+    source1 = odo(df_salary_gb[["teamID", "avg"]], ColumnDataSource)
 
     plot1 = plt.figure(title="Salary ratio by team", x_range=list(df_salary_gb["teamID"]))
     plot1.scatter(x="teamID", y="avg", source=source1, size=20)
     plot1.xaxis.major_label_orientation = np.pi/3
 
-    df = into(pd.DataFrame, db.Salaries)
+    df = odo(db.Salaries, pd.DataFrame)
     df = df[df["teamID"] == team]
     df = df[df["yearID"] == year]
 
     df = df[["playerID","salary"]].sort('salary')
-    source_team = into(ColumnDataSource, df)
+    source_team = odo(df, ColumnDataSource)
     p_team = plt.figure(title="Salary of players for %s during %s" % (team, year),
                         x_range=list(df["playerID"]))#, tools=TOOLS)
     p_team.scatter(x="playerID", y="salary", source=source_team, size=20)
